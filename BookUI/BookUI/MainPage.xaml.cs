679fb1bbe33b2bbff03a2b82fdb4697a399e4bba
Jan Kevin Cadampog <jkcadampog.rdpi@gmail.com>
2022-03-02 12:00:36 +0800

    Add project files.

---
+++ BookUI/BookUI/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BookUI
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            NavigationPage.SetHasNavigationBar(this, false);
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+           await Navigation.PushAsync(new RoomPage());
+        }
+    }
+}
